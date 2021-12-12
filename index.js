dee67bb85aafcf9351d6b6f2c885358a8e2c0b2c
M R V I K O <vikodk67@gmail.com>
2021-12-12 23:41:18 +0700

    terbaru nichh buat belajar yachh

---
+++ index.js
@@ -0,0 +1,455 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+
+try {
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+}
+catch(e) {
+  console.error(e);
+  $('.no-browser-support').show();
+  $('.app').hide();
+}
+
+
+var noteTextarea = $('#note-textarea');
+var instructions = $('#recording-instructions');
+var notesList = $('ul#notes');
+
+var noteContent = '';
+
+var notes = getAllNotes();
+renderNotes(notes);
+
+// template made by Sajad Hashemian
+// Javascript by viko menggunakan switch case
+// thanks to Ikhya - viko - zhirr - zeks
+
+// kasih creditsnya kaliii
+const version = 1.2
+var apikey = "rxking" // viko-api.herokuapp.com 
+var apikey_token = "8OoDbSQm"
+
+const BOT_IMG = "./profil.png";
+const PERSON_IMG = "http://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png";
+const BOT_NAME = "Micanss V" + version;
+const PERSON_NAME = "You";
+//audio notifnya
+var audioer = new Audio("audio/error.wav");
+var audiochat = new Audio("audio/chat-notif.wav");
+// end audio
+
+//panel chatnya
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const msgText = `${msgerInput.value}`
+  const chtar = msgerInput.value
+  const args = chtar.trim().split(/ +/).slice(1)
+  function botResponse() {
+  switch(chtar){
+	  case '#menu':
+	   const botku = `Silahkan klik icon chat pojok kanan atas<br>Version: ${version} BETA<br>
+	   `
+      console.log("Sukses tanpa error")
+  const msg000 = botku;
+  const delay000 = msg000.split(" ").length * 100;
+
+  setTimeout(() => {
+    appendMessage(BOT_NAME, BOT_IMG, "left", msg000);
+  }, delay000);
+
+	  break;
+	  case '#cerpen':
+	 fetch(`https://viko-api.herokuapp.com/api/cerpen/random?apikey=${apikey}`)
+	 .then(response => response.json())
+    .then(data => {
+        console.log(data)
+   const mainmenu = data.result
+  console.log("Sukses tanpa error")
+  const msg001 = mainmenu;
+  const delay001 = msg001.split(" ").length * 100;
+  const loading = "Loading, tunggu beberapa menit untuk mencari berkas di perpustakaan"
+  appendMessage(BOT_NAME, BOT_IMG, "left", loading);
+  setTimeout(() => {
+    appendMessage(BOT_NAME, BOT_IMG, "left", msg001);
+  }, delay001);
+
+    })
+    .catch(err => {
+     audioer.play();
+        alert("Kesalahan saat mencari cerita pendek: " + err)
+    appendMessage(BOT_NAME, BOT_IMG, "left", err);
+    });
+	  break;
+	  case '#admin':
+	  case '#owner':
+	   const admin_bot = `
+<style>
+.asuw {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 10px 22px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
+	   <strong>Viko dwi kurniawan</strong><br>
+     <a>Developer COPAS</a><br><br>
+	    <a class="asuw" href="https://wa.me/6281515958390/">Whatsapp</a>&nbsp;<a class="asuw" href="https://viko-api.herokuapp.com/">Rest API</a>
+	   `
+      console.log("Sukses tanpa error")
+  const msg003 = admin_bot;
+  const delay003 = msg003.split(" ").length * 100;
+
+  setTimeout(() => {
+    appendMessage(BOT_NAME, BOT_IMG, "left", msg003);
+  }, delay003);
+
+	  break;
+    case '#play':
+    const hasilny = prompt("masukan keyword pencarian youtube mp3");
+     if (hasilny == "null") {
+  appendMessage(BOT_NAME, BOT_IMG, "left", 'Membatalkan');
+         }
+    comd = "Silahkan tunggu " + hasilny
+    appendMessage(BOT_NAME, BOT_IMG, "left", comd);
+   fetch(`https://viko-api.herokuapp.com/api/yt/playmp3?query=${hasilny}&apikey=${apikey}`)
+   .then(response => response.json())
+    .then(data => {
+     
+      if (data.result == "masukkan parameter query") {
+  appendMessage(BOT_NAME, "left", 'masukan keywordnya kosong');
+         }
+        console.log(data)
+   const kuotes = `<img src="${data.thumb}"
+     width="300" 
+     height="150"</img><br><br><h4>${data.title}</h4>Channel: ${data.channel}<br>Viewer: ${data.views}<br>Publish: ${data.published}<br><br><iframe scrolling="no" height="50" width="300" src="${data.url}" title="${data.title}"></iframe><br>`
+  console.log("Sukses tanpa error")
+  const msg004 = kuotes;
+  const delay004 = msg004.split(" ").length * 100;
+  setTimeout(() => {
+    appendMessage(BOT_NAME, BOT_IMG, "left", msg004);
+  }, delay004);
+  if (data.statusCode) {
+  appendMessage(BOT_NAME, BOT_IMG, "left", 'Terjadi kesalahan, server tiba tiba menutup laman');
+}
+    })
+    .catch(err => {
+     audioer.play();
+        console.log("Kesalahan: " + err)
+    appendMessage(BOT_NAME, BOT_IMG, "left", err);
+    });
+    break;
+	case '#tiktok':
+    const urlnya = prompt("masukan URL tiktok!!");
+     if (urlnya == "null") {
+  appendMessage(BOT_NAME, BOT_IMG, "left", 'Membatalkan');
+         }
+    comd = "Silahkan tunggu " + urlnya
+    appendMessage(BOT_NAME, BOT_IMG, "left", comd);
+   fetch(`https://hadi-api.herokuapp.com/api/tiktok?url=${urlnya}`)
+   .then(response => response.json())
+    .then(data => {
+     
+      if (urlnya == " ") {
+  appendMessage(BOT_NAME, "left", 'masukan keywordnya kosong');
+         }
+        console.log(data)
+   const katese = `<iframe scrolling="no" height="200" width="300" src="${data.result.video.original}" title="micanssbot_tiktok"></iframe><br>
+<style>
+.asuw {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 3px 15px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 13px;
+}
+</style>
+<center><strong>untuk mendownload silahkan pilih dibawah ini</strong></center>
+<center><h5>By Hadi tiktok ${version}</h5></center>
+<br><br>
+<center><a class="asuw" href="${data.result.video.nowm}">
+<lord-icon
+    src="https://cdn.lordicon.com/tdxypxgp.json"
+    trigger="loop"
+    colors="primary:#9cf4a7,secondary:#30e849"
+    stroke="100"
+    style="width:30px;height:30px">
+</lord-icon>Unduh NoWM</a>
+<a class="asuw" href="${data.result.audio_only.audio1}">
+<lord-icon
+    src="https://cdn.lordicon.com/vieswnpa.json"
+    trigger="loop"
+    colors="primary:#30e849,secondary:#9cf4a7"
+    stroke="96"
+    style="width:30px;height:30px">
+</lord-icon>Unduh MP3</a></center>`
+  console.log("Sukses tanpa error")
+  const msg005 = katese;
+  const delay005 = msg005.split(" ").length * 100;
+  setTimeout(() => {
+    appendMessage(BOT_NAME, BOT_IMG, "left", msg005);
+  }, delay005);
+  if (data.statusCode) {
+  appendMessage(BOT_NAME, BOT_IMG, "left", 'Terjadi kesalahan, server tiba tiba menutup laman');
+}
+    })
+    .catch(err => {
+     audioer.play();
+        console.log("Kesalahan: " + err)
+    appendMessage(BOT_NAME, BOT_IMG, "left", err);
+    });
+    break;
+	  default:
+	  fetch(`https://viko-api.herokuapp.com/api/f/simi?apikey=${apikey}&query=${chtar}`)
+	 .then(response => response.json())
+    .then(data => {
+        console.log(data)
+  const mainsimi = data.result
+  const chatsim = mainsimi;
+  const delaykale = chatsim.split(" ").length * 100;
+  var audio = new Audio("audio/chat-notif.wav");
+  audiochat.play();
+  setTimeout(() => {
+    appendMessage(BOT_NAME, BOT_IMG, "left", chatsim);
+  }, delaykale);
+  var consolw = "jumlah kata diucapkan oleh micans " + mainsimi.length + "Kata"
+  console.log(consolw)
+    })
+    .catch(err => {
+     audioer.play();
+     const erorku = `<h4>periksa koneksi internet anda :(</h4> ` + err
+    appendMessage(BOT_NAME, BOT_IMG, "left", erorku);
+    });
+	  break;
+  }
+}
+  if (!msgText) return;
+
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+  msgerInput.value = "";
+
+  botResponse();
+});
+
+function appendMessage(name, img, side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// If false, the recording will stop after a few seconds of silence.
+// When true, the silence period is longer (about 15 seconds),
+// allowing us to keep recording even when the user pauses. 
+recognition.continuous = true;
+
+// This block is called every time the Speech APi captures a line. 
+recognition.onresult = function(event) {
+
+  // event is a SpeechRecognitionEvent object.
+  // It holds all the lines we have captured so far. 
+  // We only need the current one.
+  var current = event.resultIndex;
+
+  // Get a transcript of what was said.
+  var transcript = event.results[current][0].transcript;
+
+  // Add the current transcript to the contents of our Note.
+  // There is a weird bug on mobile, where everything is repeated twice.
+  // There is no official solution so far so we have to handle an edge case.
+  var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+
+  if(!mobileRepeatBug) {
+    noteContent += transcript;
+    noteTextarea.val(noteContent);
+  }
+};
+
+recognition.onstart = function() { 
+  instructions.text('Voice recognition activated. Try speaking into the microphone.');
+}
+
+recognition.onspeechend = function() {
+  instructions.text('You were quiet for a while so voice recognition turned itself off.');
+}
+
+recognition.onerror = function(event) {
+  if(event.error == 'no-speech') {
+    instructions.text('No speech was detected. Try again.');  
+  };
+}
+
+
+
+/*-----------------------------
+      App buttons and input 
+------------------------------*/
+
+$('#start-record-btn').on('click', function(e) {
+  if (noteContent.length) {
+    noteContent += ' ';
+  }
+  recognition.start();
+});
+
+
+$('#pause-record-btn').on('click', function(e) {
+  recognition.stop();
+  instructions.text('Voice recognition di berhentikan');
+});
+
+// Sync the text inside the text area with the noteContent variable.
+noteTextarea.on('input', function() {
+  noteContent = $(this).val();
+})
+
+$('#save-note-btn').on('click', function(e) {
+  recognition.stop();
+
+  if(!noteContent.length) {
+    instructions.text('Could not save empty note. Please add a message to your note.');
+  }
+  else {
+    // Save note to localStorage.
+    // The key is the dateTime with seconds, the value is the content of the note.
+    saveNote(new Date().toLocaleString(), noteContent);
+
+    // Reset variables and update UI.
+    noteContent = '';
+    renderNotes(getAllNotes());
+    noteTextarea.val('');
+    instructions.text('Note saved successfully.');
+  }
+      
+})
+
+
+notesList.on('click', function(e) {
+  e.preventDefault();
+  var target = $(e.target);
+
+  // Listen to the selected note.
+  if(target.hasClass('listen-note')) {
+    var content = target.closest('.note').find('.content').text();
+    readOutLoud(content);
+  }
+
+  // Delete note.
+  if(target.hasClass('delete-note')) {
+    var dateTime = target.siblings('.date').text();  
+    deleteNote(dateTime);
+    target.closest('.note').remove();
+  }
+});
+
+
+
+/*-----------------------------
+      Speech Synthesis 
+------------------------------*/
+
+function readOutLoud(message) {
+	var speech = new SpeechSynthesisUtterance();
+
+  // Set the text and voice attributes.
+	speech.text = message;
+	speech.volume = 1;
+	speech.rate = 1;
+	speech.pitch = 1;
+  
+	window.speechSynthesis.speak(speech);
+}
+
+
+
+/*-----------------------------
+      Helper Functions 
+------------------------------*/
+
+function renderNotes(notes) {
+  var html = '';
+  if(notes.length) {
+    notes.forEach(function(note) {
+      html+= `<li class="note">
+        <p class="header">
+          <span class="date">${note.date}</span>
+          <a href="#" class="listen-note" title="Listen to Note">Listen to Note</a>
+          <a href="#" class="delete-note" title="Delete">Delete</a>
+        </p>
+        <p class="content">${note.content}</p>
+      </li>`;    
+    });
+  }
+  else {
+    html = '<li><p class="content">You don\'t have any notes yet.</p></li>';
+  }
+  notesList.html(html);
+}
+
+
+function saveNote(dateTime, content) {
+  localStorage.setItem('note-' + dateTime, content);
+}
+
+
+function getAllNotes() {
+  var notes = [];
+  var key;
+  for (var i = 0; i < localStorage.length; i++) {
+    key = localStorage.key(i);
+
+    if(key.substring(0,5) == 'note-') {
+      notes.push({
+        date: key.replace('note-',''),
+        content: localStorage.getItem(localStorage.key(i))
+      });
+    } 
+  }
+  return notes;
+}
+
+
+function deleteNote(dateTime) {
+  localStorage.removeItem('note-' + dateTime); 
+}
---
+++ view.js
@@ -0,0 +1,50 @@
+__path = process.cwd()
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(__path + '/start.html')
+})
+// Audio NOTIFICATION
+router.get('/audio/chat-notif.wav', (req, res) => {
+    res.sendFile(__path + '/audio/chat-notif.wav')
+})
+router.get('/audio/error.wav', (req, res) => {
+    res.sendFile(__path + '/audio/error.wav')
+})
+// Panel HTML
+router.get('/index.js', (req, res) => {
+    res.sendFile(__path + '/index.js')
+})
+router.get('/profil.png', (req, res) => {
+    res.sendFile(__path + '/profil.png')
+})
+router.get('/css/style.css', (req, res) => {
+    res.sendFile(__path + '/css/style.css')
+})
+router.get('/icon/exclamation-circle-solid.png', (req, res) => {
+    res.sendFile(__path + '/icon/exclamation-circle-solid.png')
+})
+router.get('/icon/chat.png', (req, res) => {
+    res.sendFile(__path + '/icon/chat.png')
+})
+router.get('/icon/chat.png', (req, res) => {
+    res.sendFile(__path + '/icon/chat.png')
+})
+
+router.get('/config', (req, res) => {
+    config = {
+        status: true,
+        result: {
+            prefix : '#',
+            namabot: 'Micanss WEB',
+            namaowner: 'Viko dwi kurniawan',
+            instagram: 'vikodwik_rmx',
+            youtube : 'Studiovdk entertainment'
+        }
+    }
+    res.json(config)
+})
+
+module.exports = router
