0290e9016ba1db86f7fa2929f5c20c4fe294de99
M R V I K O <vikodk67@gmail.com>
2021-12-16 16:18:37 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ msgerForm.addEventListener("submit", event => {
         console.log(data)
    const katese = `
   <center>
-  <video width="420" height="240" controls autoplay>
+  <video width="320" height="150" controls autoplay>
   <source src="${data.result.video.original}" type="video/mp4">
   <source src="${data.result.video.original}" type="video/ogg">
   Your browser does not support the video tag.
@@ -287,13 +287,11 @@ function appendMessage(name, img, side, text) {
   const msgHTML = `
     <div class="msg ${side}-msg">
       <div class="msg-img" style="background-image: url(${img})"></div>
-
       <div class="msg-bubble">
         <div class="msg-info">
           <div class="msg-info-name">${name}</div>
           <div class="msg-info-time">${formatDate(new Date())}</div>
         </div>
-
         <div class="msg-text">${text}</div>
       </div>
     </div>
